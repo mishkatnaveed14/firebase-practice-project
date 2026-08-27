f1d2656e1671dbb3d35f695def7d02d048ec4db8
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-08-27 16:35:27 +0500

    working on firebase project

---
+++ firebase.config.js
@@ -0,0 +1,96 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+// database
+import { getFirestore } from  "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
+// change for all different projects
+const firebaseConfig = {
+  apiKey: "AIzaSyAXaJPxpvRyACZZMj7BeSOioiKPXaBYa3Y",
+  authDomain: "auth-af1f7.firebaseapp.com",
+  projectId: "auth-af1f7",
+  storageBucket: "auth-af1f7.firebasestorage.app",
+  messagingSenderId: "113791781042",
+  appId: "1:113791781042:web:d9d81deecf98adee08a1ab",
+  measurementId: "G-TLWBB28TQ0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// SIGN UP
+function signup(email, password) {
+
+  createUserWithEmailAndPassword(auth, email, password)
+
+    .then((userCredential) => {
+
+      const user = userCredential.user;
+
+      console.log(
+        user.email,
+        "===> successfully signed up"
+      );
+
+    })
+
+    .catch((error) => {
+
+      console.log(
+        error.code,
+        error.message,
+        "===> error while signing up"
+      );
+
+    });
+}
+
+function add
+
+
+// LOGIN
+function login(email, password) {
+
+  signInWithEmailAndPassword(auth, email, password)
+
+    .then((userCredential) => {
+
+      const user = userCredential.user;
+
+      console.log(
+        user.email,
+        "===> login successfully"
+      );
+
+    })
+
+    .catch((error) => {
+
+      console.log(
+        error.code,
+        error.message,
+        "===> error while logging in"
+      );
+
+    });
+}
+
+// // auth state change function
+function onAuthstate() {
+  onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+}
+export { signup, login, onAuthstate };
---
+++ index.js
@@ -0,0 +1,28 @@
+import { signup, login } from "./firebase.config.js";
+
+// =========================
+// SIGN IN
+// =========================
+
+const emailSignin = document.getElementById("signin-email");
+const passwordSignin = document.getElementById("signin-password");
+
+document.getElementById("signin-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  login(emailSignin.value, passwordSignin.value);
+});
+
+// =========================
+// SIGN UP
+// =========================
+
+const emailSignup = document.getElementById("signup-email");
+const passwordSignup = document.getElementById("signup-password");
+
+document.getElementById("signup-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  signup(emailSignup.value, passwordSignup.value);
+});
+
